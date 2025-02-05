7853d7731ade12542dcde15ba39c62e62afb3804
Avi Patel <avipatel805@gmail.com>
2025-02-04 22:40:42 -0600

    added spider and mine-collapse

--- main.py
+++ main.py
@@ -2,12 +2,35 @@ import pyautogui as pg
 import time
 
 
-def spider():
-    pass
+def spider(count):
+    pg.typewrite('x')
+    time.sleep(0.01)
+    pg.typewrite('x')
+    for _ in range(6):
+        pg.press('right')
+        time.sleep(0.08)
+
+    spider_loc = None
+    for _ in range(100):
+        spider_loc = pg.locateOnScreen('spider.jpg', confidence=0.7)
+        time.sleep(0.01)
+    if spider_loc is not None:
+        for _ in range(6):
+            pg.press('right')
+            time.sleep(0.08)
+        pg.typewrite('z')
+        time.sleep(0.01)
+        pg.typewrite('z')
 
 
 def mine_collapse():
-    pass
+    with pg.hold('down'):
+        time.sleep(3)
+    time.sleep(0.07)
+    with pg.hold('up'):
+        time.sleep(0.5)
+    with pg.hold('up') and pg.hold('left'):
+        time.sleep(0.7)
 
 
 def mine():
@@ -17,28 +40,37 @@ def mine():
     time.sleep(0.6)
 
     spider_loc = None
-    for _ in range(300):
-        spider_loc = pg.locateOnScreen('spider.png', confidence=0.7)
+    for _ in range(100):
+        spider_loc = pg.locateOnScreen('spider.jpg', confidence=0.7)
         time.sleep(0.01)
     if spider_loc is not None:
         spider()
 
     mine_loc = None
-    for _ in range(300):
+    for _ in range(100):
         mine_loc = pg.locateOnScreen('fire.png', confidence=0.7)
         time.sleep(0.01)
 
     if mine_loc is not None:
         mine_collapse()
+        return False
 
     with pg.hold('up'):
         time.sleep(0.07)
+    return True
 
 
 def main():
-    pass
+    counter = 0
+    while counter < 15:
+        if mine():
+            counter += 1
+        else:
+            counter = 0
+    with pg.hold('down'):
+        time.sleep(2.5)
+    main()
 
 
 if __name__ == "__main__":
     main()
-
